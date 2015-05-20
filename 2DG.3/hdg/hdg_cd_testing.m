635ac9d7b90731097095508df7b2dcbb759ef26e
Sachith Dunatunga <neocpp@gmail.com>
2015-05-20 04:09:00 -0400

    Started HDG solve function.
    Not sure why B^T has a negative sign in the notes, but it seems
    to be consistent with the other formulations.

---
+++ 2DG.3/hdg/hdg_cd_testing.m
@@ -0,0 +1,21 @@
+porder = 2;
+ngrid  = 9;
+
+mesh   = mkmesh_square(ngrid,ngrid,porder);
+master = mkmaster(mesh,2*porder);
+
+kappa = 1;
+c = [10,10];
+param = {kappa,c};
+source = @(p) 10*ones(size(p,1),1);
+dbc    = @(p) 0*ones(size(p,1),1);
+
+% HDG solver 
+[uh,qh,uhath]=hdg_solve(master,mesh,source,dbc,param);
+scaplot(mesh, uh);
+
+% HDG postprocessing 
+mesh1   = mkmesh_square(ngrid,ngrid,porder+1);
+master1 = mkmaster(mesh1,2*(porder+1));
+[ustarh]=hdg_postprocess(master,mesh,master1,mesh1,uh,qh);
+
--- 2DG.3/hdg/hdg_solve.m
+++ 2DG.3/hdg/hdg_solve.m
@@ -13,4 +13,99 @@ function [uh,qh,uhath]=hdg_solve(master,mesh,source,dbc,param)
 %      QH:           Approximate flux
 %      UHATH:        Approximate trace                              
 
+% equation parameters
+kappa = param{1};
+c = param{2};
 
+% map node to dof index (position in solution vector)
+
+phi1d(:,:) = master.sh1d(:,1,:);
+dphi1d(:,:) = master.sh1d(:,2,:);
+phi(:,:) = master.shap(:,1,:);
+dphidxi(:,:) = master.shap(:,2,:);
+dphideta(:,:) = master.shap(:,3,:);
+
+% components of jacobian
+xxi = dphidxi'*squeeze(mesh.dgnodes(:,1,:));
+xet = dphideta'*squeeze(mesh.dgnodes(:,1,:));
+yxi = dphidxi'*squeeze(mesh.dgnodes(:,2,:));
+yet = dphideta'*squeeze(mesh.dgnodes(:,2,:));
+detJ = (xxi.*yet - xet.*yxi);
+
+nel = size(mesh.t, 1);
+nf = size(mesh.f, 1);
+ndof = nf * numel(master.ploc1d);
+
+% build global matrix for trace values (global solve)
+for i=1:nel
+    ns = size(master.mass, 1);
+    ns1d = size(master.ploc1d, 1);
+
+    % local matrices
+    A = sparse(2*ns, 2*ns);
+    B = sparse(2*ns, ns);
+    C = sparse(2*ns, 3*ns1d);
+    D = sparse(ns, ns);
+    E = sparse(ns, 3*ns1d);
+    M = sparse(3*ns1d, 3*ns1d);
+    F = zeros(3*ns1d, 1);
+
+    % components of inverse jacobian
+    xix = diag(yet(:,i) ./ detJ(:,i));
+    etax = diag(-yxi(:,i) ./ detJ(:,i));
+    xiy = diag(-xet(:,i) ./ detJ(:,i));
+    etay = diag(xxi(:,i) ./ detJ(:,i));
+
+    % derivatives with respect to global coordinates
+    dphidx = (dphidxi*xix + dphideta*etax);
+    dphidy = (dphidxi*xiy + dphideta*etay);
+
+    % mass matrix if non-constant jacobians
+    mm = phi*diag(master.gwgh .* detJ(:, i))*phi';
+
+    elnn = 1:ns;
+
+    % 'A' matrix, (kinv*q_h, v)
+    A1d = mm / kappa;
+    A(elnn, elnn) = A1d; % xx
+    A(ns+elnn, ns+elnn) = A1d; % yy
+
+    % 'B' matrix, (u_h, div(v))
+    Bxx = dphidx*phi';
+    Byy = dphidy*phi';
+    B(elnn, elnn) = Bxx;
+    B(ns+elnn, elnn) = Byy;
+
+    % have to go over edges for C, D, E, M matrices
+
+    BT = B';
+    for j=1:3
+        edgenn = master.perm(:, j, 1); % only want ccw direction 
+        tracenn = (j-1)*ns1d + (1:ns1d)';
+        ep = mesh.dgnodes(edgenn, :, i);
+
+        % tangent vector
+        xsg = dphi1d'*ep(:,1);
+        ysg = dphi1d'*ep(:,2);
+        dsg = sqrt(ysg.*ysg + xsg.*xsg);
+
+        % outward normal
+        nepg = [ysg ./ dsg, -xsg ./ dsg];
+        scale = (master.gw1d .* dsg);
+        S = diag(scale);
+
+        % 'C' matrix, <u_hat, v.n>
+        Cx = phi1d*S*diag(nepg(:, 1))*phi1d';
+        Cy = phi1d*S*diag(nepg(:, 2))*phi1d';
+        C(edgenn, tracenn) = C(edgenn, tracenn) + Cx;
+        C(ns + edgenn, tracenn) = C(ns + edgenn, tracenn) + Cy;
+
+    end
+end
+
+% schur complement and solve global matrix for u_hat
+
+
+% local solve on each element using traces
+for i=1:nel
+end
